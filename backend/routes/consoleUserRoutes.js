798e4e444d5df06352714ba7a056663ac9fc8e7e
Mitchell Stanton <mitch@syntima.dev>
2026-02-19 10:33:29 -0500

    Uncomment register route in consoleUserRoutes

--- backend/routes/consoleUserRoutes.js
+++ backend/routes/consoleUserRoutes.js
@@ -5,7 +5,7 @@ import protect from '../middleware/authHandler.js';
 
 // uncomment the register route when creating the first user account
 // after the first user account is created, comment out the register route to prevent people from creating their own accounts
-// router.post('/register', registerUser);
+router.post('/register', registerUser);
 
 router.post('/login', loginUser);
 router.post('/createNewUser', protect, createNewUser);
